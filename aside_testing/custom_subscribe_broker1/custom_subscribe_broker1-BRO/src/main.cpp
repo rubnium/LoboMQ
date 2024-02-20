7383864aa0cf98fcef85b791233178e459918afe
rubnium <rubengomezvillegas@gmail.com>
2024-02-20 16:55:08 +0100

    Added subscribers vector and checker

--- aside_testing/custom_subscribe_broker1/custom_subscribe_broker1-BRO/src/main.cpp
+++ aside_testing/custom_subscribe_broker1/custom_subscribe_broker1-BRO/src/main.cpp
@@ -1,11 +1,37 @@
 #include <Arduino.h>
 #include <esp_now.h>
 #include <WiFi.h>
+#include <vector>
 
 #include "struct_message.h"
 
+std::vector<uint8_t*> subscribers; //stores each mac
+
+void printSubscribers() {
+  for (const auto& mac : subscribers) {
+    printf("%02X:%02X:%02X:%02X:%02X:%02X\n", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+  }
+  Serial.println();
+}
+
+bool isSubscribed(const uint8_t *mac) {
+  for (const auto& subscriber : subscribers) {
+    if (memcmp(mac, subscriber, 6) == 0) {
+      return true; //already subscribed
+    }
+  }
+  return false; //not subscribed
+}
+
 void handleSubscribeMsg(const SubscribeAnnouncement subAnnounce, const uint8_t *mac){
-  printf("Suscrito a %s por %02X:%02X:%02X:%02X:%02X:%02X\n", subAnnounce.topic, mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+  printf("Subscribed to %s by %02X:%02X:%02X:%02X:%02X:%02X\n", subAnnounce.topic, mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+  if (!isSubscribed(mac)) {
+    subscribers.push_back((uint8_t*)mac);
+  } else {
+    printf("Already subscribed\n");
+  }
+  printf("Actual subscribers:\n");
+  printSubscribers();
 }
 
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
