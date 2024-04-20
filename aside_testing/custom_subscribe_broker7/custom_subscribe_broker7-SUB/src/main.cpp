b3869442ebfe1b9b6a956c3a605af423f87cc5c6
rubnium <rubengomezvillegas@gmail.com>
2024-04-20 22:10:11 +0200

    Added logger to publish/subscribe functions and examples

--- aside_testing/custom_subscribe_broker7/custom_subscribe_broker7-SUB/src/main.cpp
+++ aside_testing/custom_subscribe_broker7/custom_subscribe_broker7-SUB/src/main.cpp
@@ -1,7 +1,3 @@
-#include <Arduino.h>
-#include <esp_now.h>
-#include <WiFi.h>
-
 #include <unnamedMQ/PubSub.h>
 
 typedef struct {
@@ -10,40 +6,43 @@ typedef struct {
 
 uint8_t brokerAddr[] = {0xC0, 0x49, 0xEF, 0xCA, 0x2B, 0x74}; //MAC destination address
 
+Elog *_logger;
+
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
-  if (isMQTTMessage(incomingData)) {
-    PayloadContent content = getMQTTPayload(incomingData);
+  if (isMQMessage(incomingData)) {
+    PayloadContent content = getMQPayload(incomingData);
     CustomPayloadStruct customPayloadStruct;
     memcpy(&customPayloadStruct, &content.content, content.contentSize);
+		_logger->log(INFO, "Received MQ message of %dB", content.contentSize);
     printf("\t- Number: %d\n", customPayloadStruct.number);
   } else {
-    Serial.println("Invalid message type received!");
+    _logger->log(ERROR, "Invalid message type received");
   }
 }
 
 void setup() {
   Serial.begin(9600);
+	_logger = initializeSerialLogger(SUBSCRIBER, INFO);
 
   WiFi.mode(WIFI_STA);
+
   //Initialize ESP-NOW and set up receive callback
   if (esp_now_init() != ESP_OK || esp_now_register_recv_cb(OnDataRecv) != ESP_OK) {
-    Serial.println("[SETUP] Error initializing ESP-NOW");
+    _logger->log(ERROR, "Couldn't initialize ESP-NOW");
     exit(1);
   }
 
   printf("\nSUBSCRIBER BOARD\n");
   Serial.println((String)"MAC Addr: "+WiFi.macAddress());
 
-  subscribe(brokerAddr, "#");
-  //sleep(2);
-  subscribe(brokerAddr, "s/+");
-  //sleep(2);
-  subscribe(brokerAddr, "s/mock");
+  subscribe(brokerAddr, "#", _logger);
+  subscribe(brokerAddr, "s/+", _logger);
+  subscribe(brokerAddr, "s/mock", _logger);
 
   sleep(20);
 
-  unsubscribe(brokerAddr, "#");
-  unsubscribe(brokerAddr, "s/+");
+  unsubscribe(brokerAddr, "#", _logger);
+  unsubscribe(brokerAddr, "s/+", _logger);
 }
 
 void loop() { }
