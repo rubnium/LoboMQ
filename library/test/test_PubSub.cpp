a1d7d2960f71e115396d3c2c498254fcd3b7198c
rubnium <rubengomezvillegas@gmail.com>
2024-07-11 08:44:34 +0200

    Created pubsub test

--- library/test/test_PubSub.cpp
+++ library/test/test_PubSub.cpp
@@ -1,6 +1,9 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#define DOCTEST_CONFIG_NO_POSIX_SIGNALS
+#include "doctest.h"
 #include "LoboMQ/PubSub.cpp"
-#include <unity.h>
 
+//Every topic case
 char *startingTrailingSlashTopic = "/aaa/";
 char *onlyTopic = "aaa";
 char *trailingPlusTopic = "aaa/+";
@@ -17,190 +20,75 @@ char *nullTopic;
 char *nonASCIITopic = "ááá";
 char *bigTopic = "aaaaaaaaaaaaaaaaaaaaaaaaaa";
 
+//Payload structure
 typedef struct {
   int number;
 } CustomPayloadStruct;
 
-void createMessageAndCheckAndGetContent() {
-	int number = 666;
+TEST_CASE("Create Message And Check And Get Content") {
+  int number = 666;
 
 	//Create and fill publish message
-	CustomPayloadStruct payloadSend;
-	payloadSend.number = number;
-	PublishContent pubMsg;
-	pubMsg.type = MSGTYPE_PUBLISH;
-	strcpy(pubMsg.topic, "test");
-	pubMsg.contentSize = sizeof(payloadSend);
-	memcpy(&pubMsg.content, &payloadSend, sizeof(payloadSend));
+  CustomPayloadStruct payloadSend;
+  payloadSend.number = number;
+  PublishContent pubMsg;
+  pubMsg.type = MSGTYPE_PUBLISH;
+  strcpy(pubMsg.topic, "test");
+  pubMsg.contentSize = sizeof(payloadSend);
+  memcpy(&pubMsg.content, &payloadSend, sizeof(payloadSend));
 
-	//Transform to bytes
-	const uint8_t *data = (const uint8_t *) &pubMsg;
+  //Transform to bytes
+  const uint8_t *data = (const uint8_t *) &pubMsg;
 
-	TEST_ASSERT_EQUAL(isLMQMessage(data), true);
+  CHECK(isLMQMessage(data) == true);
 
-	//Extract payload from bytes
-	PayloadContent contentRecv = getLMQPayload(data);
+  //Extract payload from bytes
+  PayloadContent contentRecv = getLMQPayload(data);
   CustomPayloadStruct payloadRecv;
   memcpy(&payloadRecv, &contentRecv.content, contentRecv.contentSize);
 
-  TEST_ASSERT_EQUAL(payloadRecv.number, number);
+  CHECK(payloadRecv.number == number);
 }
 
-
-void pubCheckStartingTrailingSlashTopic() {
-	TEST_ASSERT_EQUAL(LMQ_ERR_SUCCESS, pubTopicCheck(startingTrailingSlashTopic));
+TEST_CASE("Publish Topic Checks") {
+  CHECK(pubTopicCheck(startingTrailingSlashTopic) == LMQ_ERR_SUCCESS);
+  CHECK(pubTopicCheck(onlyTopic) == LMQ_ERR_SUCCESS);
+  CHECK(pubTopicCheck(trailingPlusTopic) == LMQ_ERR_INVAL_TOPIC);
+  CHECK(pubTopicCheck(middlePlusTopic) == LMQ_ERR_INVAL_TOPIC);
+  CHECK(pubTopicCheck(startingPlusTopic) == LMQ_ERR_INVAL_TOPIC);
+  CHECK(pubTopicCheck(onlyPlusTopic) == LMQ_ERR_INVAL_TOPIC);
+  CHECK(pubTopicCheck(trailingHashTopic) == LMQ_ERR_INVAL_TOPIC);
+  CHECK(pubTopicCheck(middleHashTopic) == LMQ_ERR_INVAL_TOPIC);
+  CHECK(pubTopicCheck(startingHashTopic) == LMQ_ERR_INVAL_TOPIC);
+  CHECK(pubTopicCheck(onlyHashTopic) == LMQ_ERR_INVAL_TOPIC);
+  CHECK(pubTopicCheck(onlySlashTopic) == LMQ_ERR_INVAL_TOPIC);
+  CHECK(pubTopicCheck(emptyTopic) == LMQ_ERR_INVAL_TOPIC);
+  CHECK(pubTopicCheck(nullTopic) == LMQ_ERR_INVAL_TOPIC);
+  CHECK(pubTopicCheck(nonASCIITopic) == LMQ_ERR_INVAL_TOPIC);
+  CHECK(pubTopicCheck(bigTopic) == LMQ_ERR_INVAL_TOPIC);
 }
 
-void pubCheckOnlyTopic() {
-	TEST_ASSERT_EQUAL(LMQ_ERR_SUCCESS, pubTopicCheck(onlyTopic));
-}
-
-void pubCheckTrailingPlusTopic() {
-	TEST_ASSERT_EQUAL(LMQ_ERR_INVAL_TOPIC, pubTopicCheck(trailingPlusTopic));
-}
-
-void pubCheckMiddlePlusTopic() {
-	TEST_ASSERT_EQUAL(LMQ_ERR_INVAL_TOPIC, pubTopicCheck(middlePlusTopic));
-}
-
-void pubCheckStartingPlusTopic() {
-	TEST_ASSERT_EQUAL(LMQ_ERR_INVAL_TOPIC, pubTopicCheck(startingPlusTopic));
-}
-
-void pubCheckOnlyPlusTopic() {
-	TEST_ASSERT_EQUAL(LMQ_ERR_INVAL_TOPIC, pubTopicCheck(onlyPlusTopic));
-}
-
-void pubCheckTrailingHashTopic() {
-	TEST_ASSERT_EQUAL(LMQ_ERR_INVAL_TOPIC, pubTopicCheck(trailingHashTopic));
-}
-
-void pubCheckMiddleHashTopic() {
-	TEST_ASSERT_EQUAL(LMQ_ERR_INVAL_TOPIC, pubTopicCheck(middleHashTopic));
-}
-
-void pubCheckStartingHashTopic() {
-	TEST_ASSERT_EQUAL(LMQ_ERR_INVAL_TOPIC, pubTopicCheck(startingHashTopic));
-}
-
-void pubCheckOnlyHashTopic() {
-	TEST_ASSERT_EQUAL(LMQ_ERR_INVAL_TOPIC, pubTopicCheck(onlyHashTopic));
-}
-
-void pubCheckOnlySlashTopic() {
-	TEST_ASSERT_EQUAL(LMQ_ERR_INVAL_TOPIC, pubTopicCheck(onlySlashTopic));
-}
-
-void pubCheckEmptyTopic() {
-	TEST_ASSERT_EQUAL(LMQ_ERR_INVAL_TOPIC, pubTopicCheck(emptyTopic));
-}
-
-void pubCheckNullTopic() {
-	TEST_ASSERT_EQUAL(LMQ_ERR_INVAL_TOPIC, pubTopicCheck(nullTopic));
-}
-
-void pubCheckNonASCIITopic() {
-	TEST_ASSERT_EQUAL(LMQ_ERR_INVAL_TOPIC, pubTopicCheck(nonASCIITopic));
-}
-
-void pubCheckBigTopic() {
-	TEST_ASSERT_EQUAL(LMQ_ERR_INVAL_TOPIC, pubTopicCheck(bigTopic));
-}
-
-void subCheckStartingTrailingSlashTopic() {
-	TEST_ASSERT_EQUAL(LMQ_ERR_SUCCESS, subTopicCheck(startingTrailingSlashTopic));
-}
-
-void subCheckOnlyTopic() {
-	TEST_ASSERT_EQUAL(LMQ_ERR_SUCCESS, subTopicCheck(onlyTopic));
-}
-
-void subCheckTrailingPlusTopic() {
-	TEST_ASSERT_EQUAL(LMQ_ERR_SUCCESS, subTopicCheck(trailingPlusTopic));
-}
-
-void subCheckMiddlePlusTopic() {
-	TEST_ASSERT_EQUAL(LMQ_ERR_SUCCESS, subTopicCheck(middlePlusTopic));
-}
-
-void subCheckStartingPlusTopic() {
-	TEST_ASSERT_EQUAL(LMQ_ERR_SUCCESS, subTopicCheck(startingPlusTopic));
-}
-
-void subCheckOnlyPlusTopic() {
-	TEST_ASSERT_EQUAL(LMQ_ERR_SUCCESS, subTopicCheck(onlyPlusTopic));
-}
-
-void subCheckTrailingHashTopic() {
-	TEST_ASSERT_EQUAL(LMQ_ERR_SUCCESS, subTopicCheck(trailingHashTopic));
-}
-
-void subCheckMiddleHashTopic() {
-	TEST_ASSERT_EQUAL(LMQ_ERR_INVAL_TOPIC, subTopicCheck(middleHashTopic));
-}
-
-void subCheckStartingHashTopic() {
-	TEST_ASSERT_EQUAL(LMQ_ERR_INVAL_TOPIC, subTopicCheck(startingHashTopic));
-}
-
-void subCheckOnlyHashTopic() {
-	TEST_ASSERT_EQUAL(LMQ_ERR_SUCCESS, subTopicCheck(onlyHashTopic));
-}
-
-void subCheckOnlySlashTopic() {
-	TEST_ASSERT_EQUAL(LMQ_ERR_INVAL_TOPIC, subTopicCheck(onlySlashTopic));
-}
-
-void subCheckEmptyTopic() {
-	TEST_ASSERT_EQUAL(LMQ_ERR_INVAL_TOPIC, subTopicCheck(emptyTopic));
-}
-
-void subCheckNullTopic() {
-	TEST_ASSERT_EQUAL(LMQ_ERR_INVAL_TOPIC, subTopicCheck(nullTopic));
-}
-
-void subCheckNonASCIITopic() {
-	TEST_ASSERT_EQUAL(LMQ_ERR_INVAL_TOPIC, subTopicCheck(nonASCIITopic));
-}
-
-void subCheckBigTopic() {
-	TEST_ASSERT_EQUAL(LMQ_ERR_INVAL_TOPIC, subTopicCheck(bigTopic));
+TEST_CASE("Subscribe Topic Checks") {
+  CHECK(subTopicCheck(startingTrailingSlashTopic) == LMQ_ERR_SUCCESS);
+  CHECK(subTopicCheck(onlyTopic) == LMQ_ERR_SUCCESS);
+  CHECK(subTopicCheck(trailingPlusTopic) == LMQ_ERR_SUCCESS);
+  CHECK(subTopicCheck(middlePlusTopic) == LMQ_ERR_SUCCESS);
+  CHECK(subTopicCheck(startingPlusTopic) == LMQ_ERR_SUCCESS);
+  CHECK(subTopicCheck(onlyPlusTopic) == LMQ_ERR_SUCCESS);
+  CHECK(subTopicCheck(trailingHashTopic) == LMQ_ERR_SUCCESS);
+  CHECK(subTopicCheck(middleHashTopic) == LMQ_ERR_INVAL_TOPIC);
+  CHECK(subTopicCheck(startingHashTopic) == LMQ_ERR_INVAL_TOPIC);
+  CHECK(subTopicCheck(onlyHashTopic) == LMQ_ERR_SUCCESS);
+  CHECK(subTopicCheck(onlySlashTopic) == LMQ_ERR_INVAL_TOPIC);
+  CHECK(subTopicCheck(emptyTopic) == LMQ_ERR_INVAL_TOPIC);
+  CHECK(subTopicCheck(nullTopic) == LMQ_ERR_INVAL_TOPIC);
+  CHECK(subTopicCheck(nonASCIITopic) == LMQ_ERR_INVAL_TOPIC);
+  CHECK(subTopicCheck(bigTopic) == LMQ_ERR_INVAL_TOPIC);
 }
 
 void setup() {
-	UNITY_BEGIN();
-	RUN_TEST(createMessageAndCheckAndGetContent);
-	RUN_TEST(pubCheckStartingTrailingSlashTopic);
-	RUN_TEST(pubCheckOnlyTopic);
-	RUN_TEST(pubCheckTrailingPlusTopic);
-	RUN_TEST(pubCheckMiddlePlusTopic);
-	RUN_TEST(pubCheckStartingPlusTopic);
-	RUN_TEST(pubCheckOnlyPlusTopic);
-	RUN_TEST(pubCheckTrailingHashTopic);
-	RUN_TEST(pubCheckMiddleHashTopic);
-	RUN_TEST(pubCheckStartingHashTopic);
-	RUN_TEST(pubCheckOnlyHashTopic);
-	RUN_TEST(pubCheckOnlySlashTopic);
-	RUN_TEST(pubCheckEmptyTopic);
-	RUN_TEST(pubCheckNullTopic);
-	RUN_TEST(pubCheckNonASCIITopic);
-	RUN_TEST(pubCheckBigTopic);
-	RUN_TEST(subCheckStartingTrailingSlashTopic);
-	RUN_TEST(subCheckOnlyTopic);
-	RUN_TEST(subCheckTrailingPlusTopic);
-	RUN_TEST(subCheckMiddlePlusTopic);
-	RUN_TEST(subCheckStartingPlusTopic);
-	RUN_TEST(subCheckOnlyPlusTopic);
-	RUN_TEST(subCheckTrailingHashTopic);	
-	RUN_TEST(subCheckMiddleHashTopic);
-	RUN_TEST(subCheckStartingHashTopic);
-	RUN_TEST(subCheckOnlyHashTopic);
-	RUN_TEST(subCheckOnlySlashTopic);
-	RUN_TEST(subCheckEmptyTopic);
-	RUN_TEST(subCheckNullTopic);
-	RUN_TEST(subCheckNonASCIITopic);
-	RUN_TEST(subCheckBigTopic);
-	UNITY_END();
+	Serial.begin(9600);
+  Serial.println("Setup complete. Starting tests...");
 }
 
 void loop() {}
